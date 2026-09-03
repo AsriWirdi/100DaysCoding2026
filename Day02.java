4735834699817d5b77b7cb0d1facb8811f98d1a4
Asri Wirdi <116133473+AsriWirdi@users.noreply.github.com>
2026-09-03 20:22:59 +0800

    Add biodata display in Day02.java

---
+++ Day02.java
@@ -0,0 +1,9 @@
+public class Day02{
+  public static void main(String... args){
+    System.out.println("BIODATA");
+    System.out.print("NAMA: ");
+    
+    System.out.println("ASRI WIRDI");
+    System.out.printf("PRODI: %s\nANGKATAN: %d\nIPK: %.1f", "INFORMATIKA", 2024, 4.8);
+  } 
+}
